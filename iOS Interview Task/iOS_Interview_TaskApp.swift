c426d2b40a61c430ccebb2fd2df5eacb6cc967de
Kareem Gamal Badr <kareem.gamal.badr@gmail.com>
2025-02-04 11:04:31 +0200

    Initial Commit

---
+++ iOS Interview Task/iOS_Interview_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Interview_TaskApp.swift
+//  iOS Interview Task
+//
+//  Created by Kareem Badr on 04/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Interview_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS Interview TaskUITests/iOS_Interview_TaskUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  iOS_Interview_TaskUITestsLaunchTests.swift
+//  iOS Interview TaskUITests
+//
+//  Created by Kareem Badr on 04/02/2025.
+//
+
+import XCTest
+
+final class iOS_Interview_TaskUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
